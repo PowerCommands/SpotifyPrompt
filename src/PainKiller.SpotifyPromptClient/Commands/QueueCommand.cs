7cce8671c126227d14a2b1cbb96c1b11176f55e4
Harri Klingsten <harri.klingsten2@scb.se>
2025-04-22 21:40:35 +0200

    .

--- src/PainKiller.SpotifyPromptClient/Commands/QueueCommand.cs
+++ src/PainKiller.SpotifyPromptClient/Commands/QueueCommand.cs
@@ -9,7 +9,7 @@ public class QueueCommand(string identifier) : ConsoleCommandBase<CommandPromptC
     public override RunResult Run(ICommandLineInput input)
     {
         var queue = QueueManager.Default.GetQueue();
-        Writer.WriteTable(queue.Select(t => new{Artist = t.Artists.First().Name,Title = t.Name ,Album = t.Album.Name, Released = t.Album.ReleaseDate.Trim().Truncate(4," ")}));
+        Writer.WriteTable(queue.Select(t => new{Artist = t.Artists.First().Name,Title = t.Name ,Album = t.Album.Name, Released = t.Album.ReleaseDate.Trim().Truncate(4,"")}));
         return Ok();
     }
 }
