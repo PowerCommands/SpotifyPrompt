b7556af8ad662e4b5d1d765d4af054ecef98e07e
Harri Klingsten <harri.klingsten2@scb.se>
2025-04-28 07:48:14 +0200

    .

--- src/PainKiller.SpotifyPromptClient/Commands/AiCommand.cs
+++ src/PainKiller.SpotifyPromptClient/Commands/AiCommand.cs
@@ -1,6 +1,7 @@
 using PainKiller.CommandPrompt.CoreLib.Modules.StorageModule.Services;
 using PainKiller.SpotifyPromptClient.DomainObjects.Data;
 using PainKiller.SpotifyPromptClient.Managers;
+using PainKiller.SpotifyPromptClient.Services;
 
 namespace PainKiller.SpotifyPromptClient.Commands;
 
@@ -17,10 +18,32 @@ public class AiCommand(string identifier) : SelectedBaseCommand(identifier)
         var keepExisting = input.HasOption("keep-existing");
         var config = Configuration.Core.Modules.Ollama;
         var aiManager = new AIManager(config.BaseAddress, config.Port, config.Model);
+        var counter = 0;
         foreach (var artist in artists)
         {
-            if(relatedArtists.Any(a => a.Id == artist.Id) && keepExisting) continue;
-            var related = new RelatedArtist{Id = artist.Id};
+            counter++;
+            aiManager.ClearMessages();
+            Writer.Clear();
+            Writer.WriteDescription("AI", $"Enriching {artist.Name} with related artists ({counter} of {artists.Count})");
+            if (relatedArtists.Any(a => a.Id == artist.Id) && keepExisting) continue;
+            var relatedArtist = new RelatedArtist{Id = artist.Id, Name = artist.Name};
+            var aiFoundArtists = aiManager.GetSimilarArtists(artist.Name);
+            foreach (var aiFoundArtist in aiFoundArtists)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(aiFoundArtist.Trim())) continue;
+                    var foundArtist = SearchService.Default.SearchArtists(aiFoundArtist).FirstOrDefault(a => a.Name.Trim().ToLower() == aiFoundArtist.Trim().ToLower());
+                    if (foundArtist == null) continue;
+                    relatedArtist.Items.Add(foundArtist);
+                    Writer.WriteLine($"Found related artist: {foundArtist.Name}");
+                }
+                catch (Exception e)
+                {
+                    Writer.WriteWarning($"{aiFoundArtist} not found on Spotify. {e.Message}", nameof(AiCommand));
+                }
+            }
+            relatedArtistStorage.Insert(relatedArtist, a => a.Id == artist.Id);
         }
         return Ok();
     }
--- src/PainKiller.SpotifyPromptClient/DomainObjects/RelatedArtist.cs
+++ src/PainKiller.SpotifyPromptClient/DomainObjects/RelatedArtist.cs
@@ -2,6 +2,7 @@
 public class RelatedArtist : IDataObjects<ArtistSimplified>
 {
     public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public DateTime LastUpdated { get; set; }
     public List<ArtistSimplified> Items { get; set; } = [];
 }
